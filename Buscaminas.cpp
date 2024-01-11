2a63761bfd91e93b99db9f8a227ff9b079c25dec
M <69995905+mofumashu@users.noreply.github.com>
2024-01-11 00:23:35 -0400

    Update Buscaminas.cpp

--- Buscaminas.cpp
+++ Buscaminas.cpp
@@ -1,21 +1,3 @@
-/*
-Universidad Nacional Experimental de Guayana
-Estructura de Datos - Seccion 1
-Prof. Carlos Abaffy
-
-    Estudiantes:
-    Camila Quiroz - V30.577.138
-    Merry-am Blanco - V30.810.720
-    Sebastian Ortiz - V
-
-                                    BUSCAMINAS
-        Juego de estrategia donde el jugador debera descubrir todas las celdas sin bombas.
-        Si el jugador descubre una celda con bomba, el juego acabara y se reiniciara.
-        El progreso del juego puede ser guardado para que este, si desea, vuelva a jugar
-        nuevamente donde lo dejo la ultima vez.
-        Juego realizado con interfaz de CMD utilizando librerias nativas de C++.
-*/
-
 #include "LibreriaMain.h"
 #include "Interfaz.cpp" //Llamado al archivo 'Interfaz' donde se mostrara todo referente al juego graficamente.
 
